c586e771de764930cd960e3bd535f35efec7f5af
audichuang <72485427+audichuang@users.noreply.github.com>
2026-06-21 13:30:37 +0800

    fix(cask): correct aghub 2.3.0 arm dmg sha256 (CI wget race wrote the empty-file hash)

--- Casks/aghub.rb
+++ Casks/aghub.rb
@@ -2,7 +2,7 @@ cask "aghub" do
   arch arm: "aarch64", intel: "x64"
 
   version "2.3.0"
-  sha256 arm:   "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+  sha256 arm:   "f6967b43a5a263f4f9baeead8dfacb5a1db3ccbbdbaf3c60a53dfd0307af2450",
          intel: "e7c163e80d1c665c5286023660ad5a4ca0c72f4a31ad587e962ccaa3d880ea11"
 
   url "https://github.com/audichuang/aghub/releases/download/v#{version}/aghub_#{version}_#{arch}.dmg"
